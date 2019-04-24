d9d3e2e89a43669ead7e15c242514d1a0ee6bca8
Justin <justinhwu95@gmail.com>
2019-04-24 15:25:01 -0400

    All functionality added

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,4 +1,47 @@
 class CharacterController < Sinatra::Base
 
 	set :views, "app/views/character"
+
+	get '/characters' do
+		@characters = Character.all
+		erb :index
+	end
+
+	get '/characters/new' do
+		erb :new
+	end
+
+	get '/characters/:id' do
+		@character = Character.find(params[:id])
+		erb :show
+	end
+
+	post '/characters' do
+		character = params["movie"]["character"]
+
+		@character = Character.create(name: character["name"],
+																	actor_name: character["actor_name"],
+																	movie_id: character["movie_id"])
+		redirect to '/characters'
+	end
+
+	get '/characters/:id/edit' do
+		@character = Character.find(params[:id])
+		erb :edit
+	end
+
+	patch '/characters/:id' do
+		character = params["movie"]["character"]
+		@character = Character.find(params[:id])
+		@character.update(name: character["name"],
+											actor_name: character["actor_name"])
+		redirect to "/characters/#{@character.id}"
+	end
+
+	delete '/characters/:id' do
+		@character = Character.find(params[:id])
+		@character.destroy
+		redirect to "/characters"
+	end
+
 end
--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -2,55 +2,57 @@ class MovieController < Sinatra::Base
 
 	set :views, "app/views/movie"
 
-	get '/movies' do 
+	get '/movies' do
 		@movies = Movie.all
 		erb :index
 	end
 
-	get '/movies/new' do 
-
+	get '/movies/new' do
 		erb :new
 	end
 
-	get '/movies/:id' do 
+	get '/movies/:id' do
 		@movie = Movie.find(params[:id])
-
 		erb :show
 	end
 
 
-	post '/movies' do 
+	post '/movies' do
 		movie = params["movie"]
-		@movie = Movie.create(title: movie["title"], 
-							description: movie["description"], 
-							genre: movie["genre"], 
-							release_date: movie["release_date"], 
+		@movie = Movie.create(title: movie["title"],
+							description: movie["description"],
+							genre: movie["genre"],
+							release_date: movie["release_date"],
 							run_time: movie["run_time"])
+		params["movie"]["character"].each do |info|
+			Character.find_or_create_by(name: info["name"],
+																	actor_name: info["actor_name"],
+																	movie_id: @movie.id)
+		end
 		redirect "/movies/#{@movie.id}"
 	end
 
-	get '/movies/:id/edit' do 
+	get '/movies/:id/edit' do
 		@movie = Movie.find(params[:id])
 
 		erb :edit
-	end 
+	end
 
-	patch '/movies/:id' do 
+	patch '/movies/:id' do
 		movie = params["movie"]
 		@movie = Movie.find(params[:id])
-		@movie.update(title: movie["title"], 
-					  description: movie["description"], 
-					  genre: movie["genre"], 
-					  release_date: movie["release_date"], 
+		@movie.update(title: movie["title"],
+					  description: movie["description"],
+					  genre: movie["genre"],
+					  release_date: movie["release_date"],
 					  run_time: movie["run_time"])
 
 		redirect "/movies/#{@movie.id}"
 	end
 
-	delete '/movies/:id' do 
+	delete '/movies/:id' do
 		@movie = Movie.find(params[:id])
 		@movie.destroy
-
 		redirect "/movies"
 	end
 
